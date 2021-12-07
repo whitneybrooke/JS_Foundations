89bd494b639f2d0af79e0c5596a2c9bf8d9bd5be
Brooke Franklin <brooke@Brookes-MacBook-Pro.local>
2021-12-07 15:55:42 -0600

    adding more small oroproblems

--- js101SmallProbs/Easy2/stringToNum.js
+++ js101SmallProbs/Easy2/stringToNum.js
@@ -24,32 +24,27 @@ the characters in the string.
 
 Examples
 
-Copy Code
 console.log(stringToInteger("4321") === 4321); // logs true
 console.log(stringToInteger("570") === 570); // logs true
 */
-//=====================================================
-
-
-//=====================================================
 
 function stringToInteger(string) {
-  for (let i = 0; i < string.length; i++) {
-    let integers = string[i * 1];
-    console.log(integers);
-  }
-  return string;
+  const DIGITS = {
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9
+  };
+  let arrayOfDigits = string.split("").map(char => DIGITS[char]);
+  let value = 0;
+  arrayOfDigits.forEach(element => (value = (10 * value) + element));
+  return value;
 }
-
-console.log(stringToInteger("4321") === 4321); // logs true
+console.log(stringToInteger("4321") === 4321);
 console.log(stringToInteger("570") === 570);
-
-// function stringToInteger(string) {
-//   for (let i = 0; i < string.length; i++) {
-//     console.log(let integer += string[i * 1]);
-//   }
-//   return integer;
-// }
-
-// console.log(stringToInteger("4321") === 4321); // logs true
-// console.log(stringToInteger("570") === 570);
